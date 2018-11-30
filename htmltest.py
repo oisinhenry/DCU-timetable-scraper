4bddde048c377c39e67199b0a10c524cb925ce82
Oisin <oisinhenry@gmail.com>
2018-11-30 16:45:35 +0000

    properly implemented cleanup function
    actually implemented the quick and dirty cleanup function.
    
    text data is now all whitespace seperated and mostly readable by humans.

--- htmltest.py
+++ htmltest.py
@@ -40,6 +40,8 @@ def print_from_url(code="CASE", year="2", sem="2"):
         url = "https://www101.dcu.ie/timetables/feed.php?prog={}&per={}&week1={}&week2={}&hour=1-20&template=student".format(code, year, "1", "12")
     elif sem == "2":
         url = "https://www101.dcu.ie/timetables/feed.php?prog={}&per={}&week1={}&week2={}&hour=1-20&template=student".format(code, year, "1", "12")
+    else:
+        url = "https://www101.dcu.ie/timetables/feed.php?prog=CASE&per=2&week1=1&week2=12&hour=1-20&template=student"
     # TODO put in weeks for semester 2
 
     html = requests.get(url, verify=False)
@@ -47,8 +49,18 @@ def print_from_url(code="CASE", year="2", sem="2"):
     html = str(html)
     # html = html.read()
     text = text_maker.handle(html)
+    text = cleanup(text)
     print(text)
     print_to_file(text)
+
+
+def cleanup(s):
+    s = s.replace("|", "")
+    s = s.replace("---", "")
+    s = s.replace("![]", "")
+    s = s.replace("(http://www.dcu.ie/images/space.gif)", "emptySlot\n")
+    return s
+
 
 
 
